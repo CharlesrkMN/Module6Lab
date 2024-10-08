f35745c26407bd29d33bad24794f22afc50aa426
CharlesrkMN <112837649+CharlesrkMN@users.noreply.github.com>
2024-10-08 22:34:29 +0000

    Initial Commit

---
+++ Module6Lab/SuperPlayer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Module6Lab
+{
+    internal class SuperPlayer : Player
+    {
+        // Additional properties or methods specific to SuperPlayer
+        // For example:
+        public void Fly()
+        {
+            Console.WriteLine("SuperPlayer is flying!");
+        }
+    }
+}
